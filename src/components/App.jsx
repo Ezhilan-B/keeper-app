0cb031660b8685fa1eda8692fbe5f2f2c0bf25cf
Ezhilan B <69838801+Ezhilan-B@users.noreply.github.com>
2023-07-28 13:36:15 +0530

    Added log for get request in App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,7 @@ function App() {
   axios.get("https://keeper-app-backend-sci5.onrender.com/noteList").then(function (response) {
     // handle success
     const updatedNoteList = response.data;
+    console.log("Get request sent \n" + updatedNoteList);
     setNoteList(updatedNoteList);
   });
 
